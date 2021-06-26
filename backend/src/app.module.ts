03bf5162a9fb7db9e7d8aaabf7b554b851cdcc87
vgolskiy <v.golskiy@yandex.ru>
2021-06-26 12:02:59 +0300

    made auth controller

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,10 +5,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ChatGateway } from './chat.gateway';
+import { AuthService } from './auth/auth.service';
 
 @Module({
   imports: [TypeOrmModule.forRoot(), UsersModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService, ChatGateway],
+  providers: [AppService, AuthService, ChatGateway],
 })
 export class AppModule {}
--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -14,7 +14,7 @@ import { LocalStrategy } from './local.strategy';
     JwtModule.register({
       secret: jwtConstants.secret,
       signOptions: { expiresIn: '60s' },
-  }),
+    }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
   exports: [AuthService],
