ea77a1651a9bf7b1c3b19ed5bb8adf8b390642b9
vgolskiy <v.golskiy@yandex.ru>
2021-06-26 08:52:39 +0300

    doesn't map auth/login and profile

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,19 +1,25 @@
 import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
-import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { LocalAuthGuard } from './auth/guards/local-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly authService: AuthService,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @UseGuards(LocalAuthGuard)
+  @Post(':auth/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
   }
 
-  @UseGuards(AuthGuard('local'))
-  @Post('auth/login')
-  async login(@Request() req) {
+  @UseGuards(JwtAuthGuard)
+  @Get(':profile')
+  getProfile(@Request() req) {
     return req.user;
   }
 }
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async validateUser(login: string, password: string): Promise<any> {
     const user = await this.usersService.findByName(login);
@@ -12,5 +16,12 @@ export class AuthService {
       return result;
     }
     return null;
+  }
+
+  async login(user: any) {
+    const payload = { login: user.login, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
